d8944fafad459fc3442e56513c918f85f6bb4f32
24761a545766-max <24761a545766@gmail.com>
2026-02-24 22:46:51 +0530

    added new to nova

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="p-10">
       
-      <h1 className="text-4xl font-bold text-pink-600">
+      <h1 className="text-4xl font-bold text-violet-600">
         Hello I'M MALLANNA
         <img src="https://www.cinejosh.com/gallereys/movies/normal/mallanna/mallanna_081.jpg"
         alt="logo"
